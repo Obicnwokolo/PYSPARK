196ab91fa20d8c1b5ef3c3a2a07a6f7ec5962af2
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-19 16:32:10 +0000

    commit11

--- src/postgrestohive.py
+++ src/postgrestohive.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql:18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "stolen_vehicles").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "stolen_vehicles").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
 df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.stolen_vehicles")
