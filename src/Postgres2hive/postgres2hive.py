113857781804d54d533354e0d6ffb13051f834f4
Obicnwokolo <nwokolo.obinna10@gmail.com>
2024-12-19 15:53:28 +0000

    commit6

--- src/Postgres2hive/postgres2hive.py
+++ src/Postgres2hive/postgres2hive.py
@@ -3,8 +3,10 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432"
-                                             "/testdb") \
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql:18.132.73.146:5432/testdb") \
     .option("driver", "org.postgresql.Driver").option("dbtable", "stolen_vehicles") \
     .option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
+
+df.write.mode("overwrite").saveAsTable("obinna.stolen_vehicles")
+print("Successfully Load to Hive")
